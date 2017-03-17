167144d6d1baa001c63b384b1917933b8ed6a73b
Stephen Adams <sadams@redhat.com>
2017-03-17 09:31:50 -0400

    update to use rxvt-unicode-256color-ml for unicode3

--- bin/term_for_display.sh
+++ bin/term_for_display.sh
@@ -4,7 +4,7 @@ hidpi_display="eDP1"
 current_display=$(get_display.py)
 
 if [ "$current_display" == "$hidpi_display" ]; then 
-  urxvt256c -fn ""xft:TamzenForPowerline:size=16:antialias=false -letsp -1 -lsp -1
+  urxvt256c-ml -fn ""xft:TamzenForPowerline:size=16:antialias=false -letsp -1 -lsp -1
 else
-  urxvt256c
+  urxvt256c-ml
 fi
